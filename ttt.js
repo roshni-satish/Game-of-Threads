99838e7e6f6b9ffcdfcb057dd8be172dd7238981
roshni-satish <51089477+roshni-satish@users.noreply.github.com>
2020-07-24 14:19:17 +0530

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -1,11 +1,10 @@
-var board = [
-	["","",""],
-	["","",""],
-	["","",""]
-]
-
-var turn, sqId, user, computer, row, col, cw;
-const ARR_LENGTH = 3;
+var board = [0,1,2,3,4,5,6,7,8]
+var trialboard = [0,1,2,3,4,5,6,7,8]
+var bestscore = [0,1,2,3,4,5,6,7,8]
+var turn, sqId, user, computer, row, col, level, wrong_move, score;
+level=0;
+wrong_move=0;
+const ARR_LENGTH = 9;
 
 $(document).ready(function() {
 	//1 checkbox event listener
@@ -20,17 +19,24 @@ $(document).ready(function() {
 	//2 square event listener
 	$(".square").click(function() {
 		sqId = $(this).attr("id");
-		PlayerMove();
-		if(checkDraw()!=0){
-                 ComputerMove()}
-                 cw=checkWinner();
-		if(cw()!=0){
-			alert(cw+" Won the Game!!!");
+		PlayerMove();	
+		if(wrong_move==0){
+			if(checkWinner(board)){
+				Win(turn);
+			}
+			else if(checkDraw()){
+				Draw();
+			}
+			else if(level!=0){
+				ComputerMove();
+				if(checkWinner(board)){
+					Win(turn);
+				}
+			}
 		}
-		else if(checkDraw()==0){
-			alert("It's a Draw!");
+		if(level==0){
+			turn = (turn == 'X' ) ? 'O' : 'X';
 		}
-		turn = (turn == user) ? computer : user;
 	});
 
 	//reset board
@@ -38,79 +44,87 @@ $(document).ready(function() {
 		resetboard();
 	})
 
+	$(".option1").click(function() {
+		unWin();
+	})
+
+	$(".option3").click(function() {
+		unDraw();
+	})
+
+	$(".1player").click(function() {
+		Closeshift();
+		overlay.classList.add('active')
+		shift2.classList.add('active')
+		level=1;
+	})
+
+	$(".2players").click(function() {
+		Closeshift();
+		level=0;
+	})
+
+	$(".easy").click(function() {
+		Closeshift2();
+		level=1;
+	})
+
 });
 
 //player move
 function PlayerMove() {
 	if($("#"+sqId).text() == ""){
-		$("#"+sqId).text(user);
-		row = getRow(sqId);
-		col = getCol(sqId);
-		board[row][col] = user;
+		if(level!=0) turn = user;
+		$("#"+sqId).text(turn);
+		board[sqId] = turn;
 		console.log(board);
+		wrong_move=0;
 	}
 	else {
 		alert("Wrong move");
+		wrong_move=1;
 	}
 }
 
 //Random move by computer
 function ComputerMove() {
-	var random;
-	var min = 0, max = 8;
-	do{
-		random = Math.floor(Math.random() * (max+min));
-	}while($("#"+random).text() != "")
-	$("#"+random).text(computer);
-	row = getRow(random);
-	col = getCol(random);
-	board[row][col] = computer;
+	if(level==1){
+		var random;
+		var min = 0, max = 8;
+		do{
+			random = Math.floor(Math.random() * (max+min));
+		}while($("#"+random).text() != "")
+		$("#"+random).text(computer);
+		board[random] = computer;
+	}
+	turn = computer;
+	console.log(board);
 }
 
 //checking for a draw
 function checkDraw() {
 	for(var i=0; i<ARR_LENGTH; i++) {
-		for(var j=0; j<board[i].length; j++) {
-			if(board[i][j] == "")
-				{os=os+1;
-				 	return os;
-					}
-		}
-	}
-	return os; 
-}
-
-//getting row number
-function getRow(sqid1) {
-	return Math.floor(sqid1 / ARR_LENGTH) ;
-}
-
-//getting col number
-function getCol(sqid1) {
-	return sqid1 % ARR_LENGTH;
+			if(board[i] != 'X' && board[i] != 'O')
+				return false;
+		} 
+		return true;
 }
 
 //check who won
-function checkWinner() {
-	//checking rows
-	for(var i = 0; i < ARR_LENGTH; i++){
-		if(board[i][0] != "" && board[i][0] == board[i][1] && board[i][1] == board[i][2])
-			return board[i][0];
-	}
-	//checking columns
-	for(var i = 0; i < ARR_LENGTH; i++){
-		if(board[0][i] != "" && board[0][i] == board[1][i] && board[1][i] == board[2][i])
-			return board[0][i];
-	}
-	//checking diagonals
-	if(board[0][0] != "" && board[0][0] == board[1][1] && board[1][1] == board[2][2])
-		return board[0][0];
-	if(board[0][2] != "" && board[0][2] == board[1][1] && board[1][1] == board[2][0])
-		return board[0][2];
-
-	// if reached this point then return false
-	return 0;
-
+function checkWinner(board) {
+	//checking for winning combinations
+	if(
+	(board[0] == turn && board[1] == turn && board[2] == turn) ||
+	(board[3] == turn && board[4] == turn && board[5] == turn) ||
+	(board[6] == turn && board[7] == turn && board[8] == turn) ||
+	(board[0] == turn && board[3] == turn && board[6] == turn) ||
+	(board[1] == turn && board[4] == turn && board[7] == turn) ||
+	(board[2] == turn && board[5] == turn && board[8] == turn) ||
+	(board[0] == turn && board[4] == turn && board[8] == turn) ||
+	(board[6] == turn && board[4] == turn && board[2] == turn)){
+		return true;}
+	//if reached here
+	return false;
 }
 
 //resetting the board for new game
@@ -121,9 +135,50 @@ function resetboard() {
 	turn = "";
 	computer = "";
 	for(var i = 0; i < ARR_LENGTH; i++) {
-		for(var j = 0; j < board[i].length; j++) {
-			board[i][j] = "";
-		}
+		board[i] = "";
 	}
+}
+
+function Win(turn) {
+	if(level!=0) {
+	if(turn==user){
+	document.getElementById("win_popup_text").innerHTML = "You Won the game!";
+	win_popup.classList.add('active')
+	overlay.classList.add('win')}
+	else{
+	document.getElementById("win_popup_text").innerHTML = "You Lost :( ";
+	win_popup.classList.add('active')}}
+	else if(level==0){
+		document.getElementById("win_popup_text").innerHTML = turn+" Won the game!";
+		win_popup.classList.add('active')
+		overlay.classList.add('win')
+	}
+}
+
+function Closeshift() {
+	shift.classList.remove('active')
+	overlay.classList.remove('active')
+}
+
+function Closeshift2() {
+	shift2.classList.remove('active')
+	overlay.classList.remove('active')
+}
+
+function unWin() {
+	win_popup.classList.remove('active')
+	overlay.classList.remove('win')
+	resetboard();
+}
+
+function Draw() {
+	draw_popup.classList.add('active')
+	overlay.classList.add('active')
+}
+
+function unDraw() {
+	draw_popup.classList.remove('active')
+	overlay.classList.remove('active')
+	resetboard();
 }
 
